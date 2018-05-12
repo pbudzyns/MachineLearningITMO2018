4b95beefdd42c275ef3f9ba8eb93ee77f87408d5
pbudzyns <Paakhec@19>
2018-05-12 19:35:55 +0300

    project sketch for svm implementation and testing

---
+++ SVM/svm.py
@@ -0,0 +1,18 @@
+"""
+It is required to write SVM algorithm without using any libraries and count the f-measure and the confusion matrix
+using the developed model.
+Next, you need investigate what Wilcoxon test is, use this test to compare kNN and SVM algorithms
+and calculate the p-value. You should use old dataset chips.txt (from the first lab).
+You should be able to answer any theory questions about SVM and about the statistical test.
+"""
+
+class SVM:
+
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
---
+++ SVM/test_and_compare.py
@@ -0,0 +1,5 @@
+from SVM.svm import SVM
+from SVM.knn import KNN
+
+if __name__ == '__main__':
+    pass
