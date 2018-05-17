f5eb70b1ca95d9ae8a2895a3d5d3972dcf66dc95
pbudzyns <Paakhec@19>
2018-05-17 21:36:28 +0300

    add pearson metric, printing results

--- FeatureSelection/metrics.py
+++ FeatureSelection/metrics.py
@@ -1,17 +1,32 @@
 import numpy as np
+from sklearn.feature_selection import chi2 as _chi2
+from sklearn.feature_selection import f_classif as _f_classif
+from sklearn.feature_selection import mutual_info_classif
+from scipy.stats import pearsonr
 
 
-def chi2():
-    pass
+def chi2(X, y):
+    return _chi2(X, y)
 
 
-def lda():
-    pass
+def f_classif(X, y):
+    y = np.ndarray.flatten(y)
+    # y = np.reshape(y, (1, max(m, n)))
+    return _f_classif(X, y)
+
+
+def mutual(x, y):
+    return mutual_info_classif(x, y), None
 
 
 def anova():
     pass
 
 
-def pearson():
-    pass
+def pearson(x, y):
+    y = np.ndarray.flatten(y)
+    m, n = np.shape(x)
+    scores = np.zeros((n, 2))
+    for i in range(n):
+        scores[i] = pearsonr(x[:, i], y)
+    return scores[:, 0], scores[:, 1]
--- FeatureSelection/select_k_features.py
+++ FeatureSelection/select_k_features.py
@@ -1,5 +1,15 @@
 import numpy as np
-from FeatureSelection.metrics import chi2, lda, anova, pearson
+from FeatureSelection.metrics import chi2, mutual, pearson, f_classif
+from queue import PriorityQueue
+from sklearn.svm import SVC
+np.warnings.filterwarnings('ignore')
+
+from sklearn.feature_selection import SelectKBest
+
+"""
+You should implement the feature selection algorithm based on the utility metric (the Filter method).
+Implement several utility metrics and compare their performance at classification tasks.
+"""
 
 
 class SelectKFeatures:
@@ -7,12 +17,57 @@ class SelectKFeatures:
     def __init__(self, score_func, k):
         self.score_func = score_func
         self.k = k
+        self._scores = None
+        self._indexes = None
+        self._pvalues = None
 
     def fit(self, features, labels):
-        pass
+        self._scores, self._pvalues = self.score_func(features, labels)
+        self._indexes = self._get_k_best_indexes()
+        self._mask = self._get_mask()
 
-    def transform(self, features):
-        pass
+    def transform(self, X):
+        # m, n = np.shape(features)
+        # result = np.zeros((m, self.k))
+        # for i, vector in enumerate(features):
+        #     result[i-1] = vector[self._mask]
+        # X = np.array(X)
+        # mask = np.asarray(self._mask)
+        # print(mask)
+        # return X[:, mask]
+        X = np.asarray(X)
+        # print('Transform: scores from indexes', self._scores[self._indexes])
+        return X[:, self._indexes]
+
+    def _transform_vector(self, v):
+        res = []
+        for idx in self._indexes:
+            res.append(v[idx])
+
+        return res
+
+    def _get_k_best_indexes(self):
+        q = PriorityQueue()
+        for idx, score in enumerate(self._scores):
+            # q.put((-score, idx))
+            q.put((1/score, idx))
+            # if score > 2390: print('score: ', score, 'idx', idx-1)
+
+        indexes = []
+        for i in range(self.k):
+            score, val = q.get()
+            # print((score, val))
+            indexes.append(val)
+            # print('getting score: ', score, 'idx ', val)
+
+        return sorted(indexes)
+
+    def _get_mask(self):
+        scores = self._scores
+        mask = np.zeros(scores.shape, dtype=bool)
+        mask[np.argsort(scores, kind="mergesort")[-self.k:]] = 1
+
+        return mask
 
 
 def load_data(filename):
@@ -31,8 +86,25 @@ if __name__ == '__main__':
     arcene_valid_data = load_data('arcene/arcene_valid.data')
     arcene_valid_labels = load_data('arcene/arcene_valid.labels')
 
-    metrics = [chi2, lda, anova, pearson]
-    # For metric in metrics
-    # fit data reducer with metric, learn model with reduced amount of features
+    metrics = [chi2, f_classif, mutual, pearson]
+    # metrics = [pearson]
+    # featureFilter = SelectKFeatures(score_func=chi2, k=10)
+    # featureFilter.fit(arcene_train_data, arcene_train_labels)
+    # print(featureFilter.transform([arcene_train_data[0]]))
+    #
+    # test = SelectKBest(score_func=chi2, k=10)
+    # test.fit(arcene_train_data, arcene_train_labels)
+    # print(test.transform([arcene_train_data[0]]))
 
-    # validate model and check score
+    for metric in metrics:
+        featureFilter = SelectKFeatures(score_func=metric, k=6)
+        featureFilter.fit(arcene_train_data, arcene_train_labels)
+
+        new_features = featureFilter.transform(arcene_train_data)
+        new_test = featureFilter.transform(arcene_valid_data)
+
+        svm = SVC(C=1.0, kernel='rbf')
+        svm.fit(new_features, arcene_train_labels)
+        print(metric.__name__, '| accuracy:', svm.score(new_test, arcene_valid_labels),
+              ' | selected features (indexes): ', featureFilter._indexes)
+
